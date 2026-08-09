c71e714f8bc752b401fb9879ec5502f03c7d92fa
Demetrie <68021436+jeffnavy14@users.noreply.github.com>
2026-08-09 08:36:28 -0400

    ?

--- scripts/quests/outlands/Ill_Take_the_Big_Box.lua
+++ scripts/quests/outlands/Ill_Take_the_Big_Box.lua
@@ -143,7 +143,11 @@ quest.sections =
         {
             onZoneIn = function(player, prevZone)
                 if quest:getVar(player, 'Prog') == 5 then
+<<<<<<< HEAD
                     return { 1101, -1, bit.bor(xi.cutsceneFlag.RESET_CAMERA, xi.cutsceneFlag.NO_PCS, xi.cutsceneFlag.NO_NPCS) }
+=======
+                    return { 1101, -1, bit.bor(xi.cutsceneFlag.UNKNOWN_1, xi.cutsceneFlag.NO_PCS, xi.cutsceneFlag.NO_NPCS) }
+>>>>>>> parent of e4b52b4f10 (Revert "Merge remote-tracking branch 'upstream/base' into base")
                 end
             end,
 
